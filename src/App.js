672a843c6fb96459847e6297f1628cc507ba3039
jvill171 <juanvillarreal64@yahoo.com>
2023-08-15 22:31:34 -0500

    Removed unused files & updated styles for App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Nav from'./components/Nav';
+import RouteList from './components/RouteList';
 
-function App() {
+function App({dogs}) {
+
   return (
     <div className="App">
-      <h1>Good luck!</h1>
+      <BrowserRouter>
+        <Nav dogNames={dogs.map(dog => dog.name)} />
+        <RouteList dogs={dogs}/>
+      </BrowserRouter>
     </div>
   );
 }
+
+App.defaultProps = {
+  "dogs": [
+      {
+        "name": "Whiskey",
+        "age": 5,
+        "src": "whiskey",
+        "facts": [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        "name": "Duke",
+        "age": 3,
+        "src": "duke",
+        "facts": [
+          "Duke believes that ball is life.",
+          "Duke likes snow.",
+          "Duke enjoys pawing other dogs."
+        ]
+      },
+      {
+        "name": "Perry",
+        "age": 4,
+        "src": "perry",
+        "facts": [
+          "Perry loves all humans.",
+          "Perry demolishes all snacks.",
+          "Perry hates the rain."
+        ]
+      }
+    ]
+};
+
 
 export default App;
